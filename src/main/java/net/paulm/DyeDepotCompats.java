ecb29ce77d00a243074e7d6c1c30a72d5dbbb9ea
PaulMarmet <paulmarmet1002@gmail.com>
2024-06-12 21:56:50 -0400

    Initial Commit - Comforts

---
+++ src/main/java/net/paulm/DyeDepotCompats.java
@@ -0,0 +1,16 @@
+package net.paulm;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DyeDepotCompats implements ModInitializer {
+	public static final String MOD_ID = "dye_depot_c";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
